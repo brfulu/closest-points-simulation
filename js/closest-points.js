d36aa04e7344e8e31274928ede50d79c3ddc928b
branko_fu <brankofulurija@gmail.com>
2019-01-06 21:32:56 +0100

    Working version d&q

--- js/closest-points.js
+++ js/closest-points.js
@@ -4,27 +4,59 @@ export default class ClosestPoints {
 
   bruteForce(points) {
     let minDistance = Number.MAX_VALUE;
+    let pointA, pointB;
+
     for (let i = 0; i < points.length; i++) {
       for (let j = i + 1; j < points.length; j++) {
         let distance = this._getDistance(points[i], points[j]);
         if (distance < minDistance) {
           minDistance = distance;
+          pointA = points[i];
+          pointB = points[j];
         }
       }
     }
-    return minDistance;
+    return {
+      distance: minDistance,
+      pointA,
+      pointB
+    };
   }
 
   divideAndConquer(points) {
+    this.events = []
     this.points = points;
-    this.pointsOrderedByX = points.sort((a, b) => a.x - b.x);
-    this.pointsOrderedByY = points.sort((a, b) => a.y - b.y);
-    return this._closestPair(this.pointsOrderedByX);
+
+    points.sort((a, b) => a.x - b.x);
+    this.pointsOrderedByX = points.slice();
+
+    points.sort((a, b) => a.y - b.y);
+    this.pointsOrderedByY = points.slice();
+
+    let result = this._closestPair(this.pointsOrderedByX);
+    return {
+      distance: result.distance,
+      events: this.events
+    };
   }
 
   _closestPair(points) {
+    this.events.push({
+      type: 'highlightRect',
+      x: points[0].x - 6,
+      y: 0,
+      width: points[points.length - 1].x - points[0].x + 12,
+      height: 500
+    });
+
     if (points.length <= 3) {
-      return this.bruteForce(points);
+      let bruteResult = this.bruteForce(points);
+      this.events.push({
+        type: 'drawLine',
+        pointA: bruteResult.pointA,
+        pointB: bruteResult.pointB
+      });
+      return bruteResult;
     }
 
     let middleIndex = Math.floor(points.length / 2);
@@ -33,14 +65,53 @@ export default class ClosestPoints {
     let leftPoints = points.slice(0, middleIndex);
     let rightPoints = points.slice(middleIndex);
 
-    let minDistanceLeft = this._closestPair(leftPoints);
-    let minDistanceRight = this._closestPair(rightPoints);
-    let minDinstanceCombined = Math.min(minDistanceLeft, minDistanceRight);
+    let leftResult = this._closestPair(leftPoints);
+    let minDistanceLeft = leftResult.distance;
 
-    let strip = this.pointsOrderedByY.filter(point => this._getDistance(point, middlePoint));
-    let minDistanceStrip = this._getMinimumDistanceStrip(strip);
+    let rightResult = this._closestPair(rightPoints);
+    let minDistanceRight = rightResult.distance;
 
-    return Math.min(minDinstanceCombined, minDistanceStrip);
+    let combinedResult = leftResult.distance < rightResult.distance ? leftResult : rightResult;
+
+    this.events.push({
+      type: 'combine',
+      x: points[0].x - 6,
+      y: 0,
+      width: points[points.length - 1].x - points[0].x + 12,
+      height: 500,
+      pointA: combinedResult.pointA,
+      pointB: combinedResult.pointB
+    });
+
+    let strip = this.pointsOrderedByY.filter(point => {
+      return Math.abs(point.x - middlePoint.x) <= combinedResult.distance
+        && point.x >= points[0].x
+        && point.x <= points[points.length - 1].x;
+    });
+
+
+    this.events.push({
+      type: 'highlightStrip',
+      x: Math.max(points[0].x - 6, middlePoint.x - combinedResult.distance - 6),
+      y: 0,
+      width: Math.min(points[points.length - 1].x - Math.max(points[0].x, middlePoint.x - combinedResult.distance - 6), 2 * combinedResult.distance + 12),
+      height: 500,
+      pointA: combinedResult.pointA,
+      pointB: combinedResult.pointB
+    });
+    let stripResult = this._getMinimumDistanceStrip(strip);
+
+    let totalResult = combinedResult.distance < stripResult.distance ? combinedResult : stripResult;
+    this.events.push({
+      type: 'highlightResult',
+      x: points[0].x - 6,
+      y: 0,
+      width: points[points.length - 1].x - points[0].x + 12,
+      height: 500,
+      pointA: totalResult.pointA,
+      pointB: totalResult.pointB
+    });
+    return totalResult;
   }
 
   _getDistance(first, second) {
@@ -49,11 +120,26 @@ export default class ClosestPoints {
 
   _getMinimumDistanceStrip(strip) {
     let minDistance = Number.MAX_VALUE;
+    let pointA, pointB;
     for (let i = 0; i < strip.length; i++) {
       for (let j = i + 1; j < strip.length && j - i <= 15; j++) {
-        minDistance = Math.min(minDistance, this._getDistance(strip[i], strip[j]));
+        if (this._getDistance(strip[i], strip[j]) < minDistance) {
+          minDistance = this._getDistance(strip[i], strip[j]);
+          pointA = strip[i];
+          pointB = strip[j];
+        }
+
+        this.events.push({
+          type: 'highlightPoints',
+          pointA: strip[i],
+          pointB: strip[j]
+        });
       }
     }
-    return minDistance;
+    return {
+      distance: minDistance,
+      pointA,
+      pointB
+    };
   }
 }
--- js/index.js
+++ js/index.js
@@ -2,28 +2,38 @@
 
 import ClosestPoints from './closest-points';
 
-const points = [{ x: 2, y: 3 }, { x: 12, y: 30 }, { x: 40, y: 50 }, { x: 5, y: 1 }, { x: 12, y: 10 }, { x: 3, y: 4 }];
-const closestPoints = new ClosestPoints();
-console.log(points);
-console.log(closestPoints.divideAndConquer(points));
-console.log(closestPoints.bruteForce(points));
+// const testPoints = [{ x: 2, y: 3 }, { x: 12, y: 30 }, { x: 40, y: 50 }, { x: 5, y: 1 }, { x: 12, y: 10 }, { x: 3, y: 4 }];
+// const closestPoints = new ClosestPoints();
+// console.log(testPoints);
+// console.log(closestPoints.divideAndConquer(testPoints));
+// console.log(closestPoints.bruteForce(testPoints));
 
 // Drawing
 
 const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const pointSize = 7;
 
 const clearButton = document.getElementById('clear');
 const nextButton = document.getElementById('next');
 const backButton = document.getElementById('back');
+const startButton = document.getElementById('start');
 
-function drawPoint(event) {
-  var rect = canvas.getBoundingClientRect();
-  var x = event.clientX - rect.left;
-  var y = event.clientY - rect.top;
+const points = [];
+let eventIndex = -1;
+let events = [];
 
-  const ctx = canvas.getContext('2d');
+function drawOnClick(event) {
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
 
+  drawPoint(x, y);
+  points.push({ x, y });
+}
+
+function drawPoint(x, y) {
+  ctx.save();
   ctx.beginPath();
   ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
 
@@ -33,12 +43,132 @@ function drawPoint(event) {
   ctx.strokeStyle = "#000000";
   ctx.lineWidth = 1;
   ctx.stroke();
+  ctx.restore();
+}
+
+function drawLine(a, b) {
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.moveTo(a.x, a.y);
+  ctx.lineTo(b.x, b.y);
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.restore();
+
+  points.forEach((p) => drawPoint(p.x, p.y));
+}
+
+function drawLabel(text, p1, p2, padding) {
+  if (p1.x > p2.x) {
+    let t = p1;
+    p1 = p2;
+    p2 = t;
+  }
+
+  let alignment = 'center';
+  let dx = p2.x - p1.x;
+  let dy = p2.y - p1.y;
+  let p = p1;
+  let pad = 1 / 2;
+
+  ctx.save();
+  ctx.textAlign = alignment;
+  ctx.fillStyle = '#4cbb50';
+  ctx.font = 'bold 20px Arial';
+  ctx.translate(p.x + dx * pad, p.y + dy * pad);
+  ctx.rotate(Math.atan2(dy, dx));
+  ctx.fillText(text, 0, 0);
+  ctx.restore();
+}
+
+function highlightPoint(x, y, color) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
+
+  ctx.fillStyle = "#0000ff";
+  ctx.fill();
+
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.restore();
+}
+
+function highlightRect(x, y, width, height, color) {
+  ctx.save();
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+  ctx.strokeRect(x, y, width, height);
+  ctx.restore();
+  points.forEach((p) => drawPoint(p.x, p.y));
 }
 
 function clearCanvas() {
-  const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  points = [];
 }
 
-canvas.addEventListener('click', drawPoint);
+function next() {
+  if (eventIndex < events.length - 1) {
+    if (eventIndex >= 0) {
+      let event = events[eventIndex];
+      if (event.type == 'highlightRect') {
+        highlightRect(event.x, event.y, event.width, event.height, '#f8f5f0');
+      } else if (event.type == 'highlightPoints') {
+        highlightPoint(event.pointA.x, event.pointA.y, '#00000');
+        highlightPoint(event.pointB.x, event.pointB.y, '#00000');
+      }
+    }
+
+    eventIndex++;
+    let event = events[eventIndex];
+    if (event.type == 'highlightRect') {
+      highlightRect(event.x, event.y, event.width, event.height, '#fef36f');
+    } else if (event.type == 'clearHighlightRect') {
+      highlightRect(event.x, event.y, event.width, event.height, '#f8f5f0');
+    } else if (event.type == 'drawLine') {
+      drawLine(event.pointA, event.pointB);
+    } else if (event.type == 'highlightPoints') {
+      highlightPoint(event.pointA.x, event.pointA.y, '#ff0000');
+      highlightPoint(event.pointB.x, event.pointB.y, '#ff0000');
+    } else if (event.type == 'combine') {
+      highlightRect(event.x, event.y, event.width, event.height, '#fef36f');
+      drawLine(event.pointA, event.pointB);
+    } else if (event.type == 'highlightStrip') {
+      highlightRect(event.x, event.y, event.width, event.height, '#4CAF50');
+      drawLine(event.pointA, event.pointB);
+    } else if (event.type == 'highlightResult') {
+      highlightRect(event.x, event.y, event.width, event.height, '#fef36f');
+      drawLine(event.pointA, event.pointB);
+    }
+  }
+}
+
+function back() {
+  if (eventIndex > 0) {
+    eventIndex--;
+    let event = events[eventIndex];
+    if (event.type == 'highlightRect') {
+      highlightRect(event.x, event.y, event.width, event.height, '#' + Math.floor(Math.random() * 16777215).toString(16));
+    } else if (event.type == 'highlightRect') {
+      highlightRect(event.x, event.y, event.width, event.height, '#f8f5f0');
+    } else if (event.type == 'drawLine') {
+      drawLine(event.pointA, event.pointB);
+    }
+  }
+}
+
+function start() {
+  const closestPoints = new ClosestPoints();
+  const result = closestPoints.divideAndConquer(points);
+  events = result.events;
+  console.log(result);
+}
+
+canvas.addEventListener('click', drawOnClick);
 clearButton.addEventListener('click', clearCanvas);
+nextButton.addEventListener('click', next);
+backButton.addEventListener('click', back);
+startButton.addEventListener('click', start);
