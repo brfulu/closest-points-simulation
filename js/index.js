ad4abc02e39870e77c4e78f71f93fc521ee304da
branko_fu <brankofulurija@gmail.com>
2019-01-06 17:34:07 +0100

    Adding points and clearing canvas

--- js/index.js
+++ js/index.js
@@ -7,3 +7,38 @@ const closestPoints = new ClosestPoints();
 console.log(points);
 console.log(closestPoints.divideAndConquer(points));
 console.log(closestPoints.bruteForce(points));
+
+// Drawing
+
+const canvas = document.getElementById('canvas');
+const pointSize = 7;
+
+const clearButton = document.getElementById('clear');
+const nextButton = document.getElementById('next');
+const backButton = document.getElementById('back');
+
+function drawPoint(event) {
+  var rect = canvas.getBoundingClientRect();
+  var x = event.clientX - rect.left;
+  var y = event.clientY - rect.top;
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.arc(x, y, pointSize, 0, Math.PI * 2, true);
+
+  ctx.fillStyle = "#0000ff";
+  ctx.fill();
+
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 1;
+  ctx.stroke();
+}
+
+function clearCanvas() {
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+canvas.addEventListener('click', drawPoint);
+clearButton.addEventListener('click', clearCanvas);
